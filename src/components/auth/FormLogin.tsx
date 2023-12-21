beb0bb225439323e7d080e59683b4c24c2c6fdb8
siska199 <siska.apriana.1703226@students.um.ac.id>
2023-12-21 09:20:55 +0700

    add loading indicator while login

--- src/components/auth/FormLogin.tsx
+++ src/components/auth/FormLogin.tsx
@@ -4,13 +4,13 @@ import Input from "../Input"
 
 
 const FormLogin = () => {
-    const {form,handleLogin,handleOnChange,disabled} = useAuth()
+    const {form,handleLogin,handleOnChange,disabled, loading} = useAuth()
 
   return (
     <section className="glass-card space-y-4 p-4 flex flex-col"  >
         <Input name={"username"} label={"USERNAME" } value={form.username} onChange={handleOnChange} />
         <Input name={"password"} label={"PASSWORD"} value={form.password} type={"password"} onChange={handleOnChange} />
-        <Button onClick={handleLogin} color={"blue"} label={"login"} customeClass="w-[50%] ml-auto" disabled={disabled}/>
+        <Button onClick={handleLogin} color={"blue"} label={loading? "Loading..." : "login"} customeClass="w-[50%] ml-auto" disabled={disabled || loading}/>
     </section>
   )
 }
--- src/hooks/auth/useAuth.tsx
+++ src/hooks/auth/useAuth.tsx
@@ -12,7 +12,8 @@ const useAuth = () => {
     })
 
     const [disabled, setDisabled] = useState(true)
-
+    const [loading, setLoading] = useState(false)
+    
     useEffect(()=>{
         const isDisabled = !(form.username && form.password);
         setDisabled(isDisabled)
@@ -27,28 +28,35 @@ const useAuth = () => {
     }
 
     const handleLogin = async ()=>{
-        const response : TResponseAPI = await new Promise((resolve) => {
-            setTimeout(() => {
-                resolve({
-                status : 200,
-                message : 'Success Login'
-                });
-            }, 1000); 
-        });
-
-
-        if(response?.status===200){
-            navigate('/universities')
-            localStorage.setItem('isAuth', 'true')
-            localStorage.setItem('user', JSON.stringify({
-                username : form.username
-            }))
-        }else{
-            alert('Anda tidak dapat login karena telah terjadi kesalahan pada sistem, harap hubungi admin')
+        try {
+            setLoading(true)
+            const response : TResponseAPI = await new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve({
+                    status : 200,
+                    message : 'Success Login'
+                    });
+                }, 1000); 
+            });
+    
+            if(response?.status===200){
+                navigate('/universities')
+                localStorage.setItem('isAuth', 'true')
+                localStorage.setItem('user', JSON.stringify({
+                    username : form.username
+                }))
+            }else{
+                alert('Anda tidak dapat login karena telah terjadi kesalahan pada sistem, harap hubungi admin')
+            }
+            
+        } catch (error:any) {
+            console.log("error: ", error.message)
+        }finally{
+            setLoading(false)
         }
     }
 
-    return {form,user,handleLogin,handleOnChange,disabled}
+    return {form,user,handleLogin,handleOnChange,disabled, loading}
 }
 
 export default useAuth
