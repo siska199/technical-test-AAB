4160c39946161e4f6af335f3bf03f874e902b5bc
siska199 <siska.apriana.1703226@students.um.ac.id>
2023-12-21 08:51:54 +0700

    fix show modal detail university

--- src/components/ContainerModal.tsx
+++ src/components/ContainerModal.tsx
@@ -8,12 +8,12 @@ const ContainerModal = (props:TProps) => {
     const { children, onCloseModal, }= props
 
   return (
-    <article className="h-full z-[99] w-full fixed top-0 left-0 flex backdrop-invert backdrop-opacity-5 bg-white/20 items-center justify-center">
-      <div className={'bg-black p-5'}>
-        <header className="sticky top-0  bg-black">
+    <article className="h-full z-[99] w-full fixed top-0 left-0 flex backdrop-invert backdrop-opacity-5 bg-black/20 items-center justify-center">
+      <div className={'p-5'}>
+        <header className="sticky top-0  ">
           <button
             onClick={onCloseModal}
-            className="p-2 h-8 w-8 cursor-pointer flex rounded-full hover:bg-zinc-700"
+            className="p-2 h-8 w-8 cursor-pointer flex rounded-full "
           >
             X
           </button>
